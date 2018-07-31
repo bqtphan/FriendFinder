8bd9707b5da26eae83cdd36e453a0b2253be8ff5
bqtphan <bqtphan@utexas.edu>
2018-07-31 11:36:33 -0500

    almost done

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friends = [
+    {
+        "name": "Britney",
+        "photo": "https://images-na.ssl-images-amazon.com/images/I/C1XxZKbHSrS._SL1000_.png",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Mila",
+        "photo": "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-img.instyle.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F684xflex%2Fpublic%2Fimages%2F2017%2F06%2F062717-mila-kunis-lead.jpg%3Fitok%3Dvekibvp8&w=700&q=85",
+        "scores": [
+            2,
+            5,
+            4,
+            4,
+            1,
+            2,
+            2,
+            2,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Melissa",
+        "photo": "https://i.kinja-img.com/gawker-media/image/upload/s--kq0gO2At--/c_scale,f_auto,fl_progressive,q_80,w_800/wu1rpkcjnwoqdj0fwosp.jpg",
+        "scores": [
+            2,
+            5,
+            4,
+            2,
+            4,
+            4,
+            1,
+            4,
+            4,
+            1
+        ]
+    }
+]
+
+module.exports = friends;
